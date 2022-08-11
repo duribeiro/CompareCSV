5adb347059327c717cc4a858259ec2245cdb66f4
Eduardo Ribeiro - IATec <eduardo.sribeiro@iatec.com>
2022-08-11 16:16:22 -0300

    Add project files.

---
+++ CompareCSV/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CompareCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_open1 = new System.Windows.Forms.Button();
+            this.btn_open2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.bt_compare = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 104);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(362, 468);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_open1
+            // 
+            this.btn_open1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_open1.Location = new System.Drawing.Point(146, 39);
+            this.btn_open1.Name = "btn_open1";
+            this.btn_open1.Size = new System.Drawing.Size(75, 23);
+            this.btn_open1.TabIndex = 0;
+            this.btn_open1.Text = "Abir CSV 1";
+            this.btn_open1.UseVisualStyleBackColor = true;
+            this.btn_open1.Click += new System.EventHandler(this.btn_open1_Click);
+            // 
+            // btn_open2
+            // 
+            this.btn_open2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_open2.Location = new System.Drawing.Point(514, 39);
+            this.btn_open2.Name = "btn_open2";
+            this.btn_open2.Size = new System.Drawing.Size(75, 23);
+            this.btn_open2.TabIndex = 1;
+            this.btn_open2.Text = "Abrir CSV 2";
+            this.btn_open2.UseVisualStyleBackColor = true;
+            this.btn_open2.Click += new System.EventHandler(this.btn_open2_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_open2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_open1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.bt_compare, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox3, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(736, 678);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(371, 104);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(362, 468);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // bt_compare
+            // 
+            this.bt_compare.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bt_compare.Location = new System.Drawing.Point(146, 615);
+            this.bt_compare.Name = "bt_compare";
+            this.bt_compare.Size = new System.Drawing.Size(75, 23);
+            this.bt_compare.TabIndex = 5;
+            this.bt_compare.Text = "Comparar";
+            this.bt_compare.UseVisualStyleBackColor = true;
+            this.bt_compare.Click += new System.EventHandler(this.bt_compare_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(371, 578);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(362, 96);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Abrir Arqvivo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 678);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Comparador de CSV";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_open1;
+        private System.Windows.Forms.Button btn_open2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button bt_compare;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ CompareCSV/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.LinkLabel;
+
+namespace CompareCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_open1_Click(object sender, EventArgs e)
+        {
+            RichTextBox rt1 = this.richTextBox1;
+            open(rt1);
+        }
+        private void btn_open2_Click(object sender, EventArgs e)
+        {
+            RichTextBox rt2 = this.richTextBox2;
+            open(rt2);
+        }
+
+        private void open (RichTextBox rt)
+        {
+            openFileDialog1.Filter = "CSV Files (*.csv)|*.csv|TXT Files (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileStream arquivo = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);  
+                    StreamReader sr = new StreamReader(arquivo);
+                    sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                    rt.Text = "";
+                    string linha = sr.ReadLine();
+                    while(linha != null)
+                    {
+                        rt.Text += linha + "\n";
+                        linha = sr.ReadLine();
+                    }
+                    sr.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro de leitura: " + ex.Message, "Erro ao ler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void bt_compare_Click(object sender, EventArgs e)
+        {
+            repeatedLines();
+        }
+
+        private void repeatedLines ()
+        {
+            // Converte a string do campo em array
+            string[] rt1 = richTextBox1.Text.Split('\n');
+            string[] rt2 = richTextBox2.Text.Split('\n');
+
+            //Fazer a comparação dos arrays
+            var crossArray = rt1.Intersect(rt2).ToArray();
+            richTextBox3.Text = "";
+            for (int i = 0; crossArray[i] != ""; i++)
+            {
+                richTextBox3.Text += crossArray[i] + "\n";
+            }
+        }
+    }
+}
